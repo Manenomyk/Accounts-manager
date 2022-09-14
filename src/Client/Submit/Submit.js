69fd5499cb64727643c91c6b1abdca1fbb5efe7d
Bahatimyk <manenomichael99@gmail.com>
2022-09-14 14:06:18 +0300

    Submit page updation

--- src/Client/Submit/Submit.js
+++ src/Client/Submit/Submit.js
@@ -9,90 +9,44 @@ function Submit() {
     return ( 
         <div>
             <Navbar />
-
-    <div className='Subbody'>
+            <div className='Subbody'>
             <Sidebar />
 
-
-        <div >
-        <sub.Container className="mb-12">
-        <h4>Questions done</h4>
-                <hr />
-
-            <sub.Container className='mb-5'>
-                    <sub.Row>
-                        <sub.Col lg={12} >
-                            <sub.Card>
-                                <div>
-                                Our Cortages are characterized by their spacious well maintained rooms
-                                            and ammenities that range from different sizes incuding rooms for one, for two and
-                                            even For a whole family.
-
-                                </div>
-
-                               
-                            
-                            <sub.Container>
-                                <sub.Row>
-                                    <sub.Col lg={6}>
-                                        {/* <sub.Card> */}
-                                        <h5>Question</h5>
-                                         <input type="text" />
-                                        {/* </sub.Card> */}
-                                    </sub.Col>
-                                    <sub.Col lg={2}>
-                                        {/* <sub.Card> */}
-                                        <h5>Date</h5>
-                                         <input type="date" />
-                                        {/* </sub.Card> */}
-                                    </sub.Col>
-                                    <sub.Col lg={2}>
-                                        {/* <sub.Card> */}
-                                        <h5>Amount</h5>
-                                         <input type="text" />
-                                        {/* </sub.Card> */}
-                                    </sub.Col>
-                                </sub.Row>
-                            </sub.Container></sub.Card>
-                        
-
-
-                        </sub.Col>       
-                    </sub.Row>
-                </sub.Container>
-
-        </sub.Container>
-
-        </div>
-
-
-
-
-
-    </div>
-<sub.Container className="mb-5" style={{background:"blue"}}>
-    <sub.Row>
-        <sub.Col lg={7}>
-            qwerty
-            <sub.Card>
+            <sub.Container className='mt-3 pt-3' id='subcont' >
                 
-                <input type="text" />
-            </sub.Card>
-        </sub.Col>
-        <sub.Col lg={3}>
-            <sub.Card>
-                qwertyui
-            </sub.Card>
-        </sub.Col>
-        <sub.Col lg={2}>
-            <sub.Card>
-                <input type="text" />
-            </sub.Card>
-        </sub.Col>
-    </sub.Row>
-</sub.Container>
+            <button className='btn btn-danger mb-4' id='Addbtn'>Add Question</button>
+                <sub.Row>
+                    <sub.Col lg={7}>
+                        <sub.Card className='card'>
+                            Question
+                            
+                            <input type="text" className='input' />
+                            
+                        </sub.Card>
+                    </sub.Col>
+                    <sub.Col lg={2}>
+                        <sub.Card>
+                            Date
+                    
+                            <input type="date" className='input'/>
+                            
+                        </sub.Card>
+                    </sub.Col>
+                    <sub.Col lg={2}>
+                        <sub.Card>
+                            Amount
+                           
+                            <input type="text" className='input'/>
+                            
+                        </sub.Card>
+                    </sub.Col>
+                    
+                </sub.Row>
+                
+            </sub.Container>
+            </div>
 
-        </div>
+     </div>
      );
 }
 
